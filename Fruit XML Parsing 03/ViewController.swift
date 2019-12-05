a26877e19c9b03dcb8f866ef776d02ea6a07c36b
dit02 <dit@imac-7.local>
2019-12-05 14:39:37 +0900

    파싱연습

--- Fruit XML Parsing 03/ViewController.swift
+++ Fruit XML Parsing 03/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    var item:[String:String] = [:]
+    var elements:[[String:String]] = []
+    var currentElement = "" //현재 태그저장
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml")
+        // 옵셔널 바인딩 체크
+        if path != nil {
+            
+           let myParser = XMLParser(contentsOf: path!)
+            myParser?.delegate = self
+            if myParser?.parse() != nil {
+                print("parsing succed")
+            } else {
+                print("parsing error")
+            }
+        } else {
+            print("file error")
+        }
+        
+        // 태그를 만날때 호출
+        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]){
+            
+            currentElement = elementName
+            print(currentElement)
+        }
+        //태그 다음에 문자를 만날때
+        func parser(_ parser: XMLParser, foundCharacters string: String){
+            // 문자 공백 제거
+            let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+            
+            if !data.isEmpty{
+                item[currentElement] = data
+            }
+        }
+        // 태그가 끝날때 호출
+        func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            if elementName == "item"{
+                elements.append(item)
+            }
+        }
     }
-
-
 }
 
